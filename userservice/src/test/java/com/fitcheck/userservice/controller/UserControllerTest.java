167119aad1afaae6a53cbccf7f6c099342e70b7e
TheSaifZaman <info.saifzaman@gmail.com>
2025-08-31 23:33:38 +0600

    UserService: Test Case Added

---
+++ userservice/src/test/java/com/fitcheck/userservice/controller/UserControllerTest.java
@@ -0,0 +1,31 @@
+package com.fitcheck.userservice.controller;
+
+import com.fitcheck.userservice.dto.RegisterRequest;
+import com.fitcheck.userservice.services.UserService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class UserControllerTest {
+
+    @Mock
+    private UserService userService;
+
+    @InjectMocks
+    private UserController userController;
+
+    @Test
+    void register() {
+        RegisterRequest request = new RegisterRequest();
+        request.setFirstName("testFirstName");
+        request.setLastName("testLastName");
+        request.setEmail("test@mail.com");
+        request.setPassword("password");
+        userController.register(request);
+        Mockito.verify(userService, Mockito.times(1)).register(request);
+    }
+}
